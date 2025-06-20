c7b70a33acba1fce4fc954a2ffcba8ae0ea21345
Catie Condit <concat2024@gmail.com>
2025-06-20 17:24:56 -0400

    Updated date attribute.

--- JS/index.js
+++ JS/index.js
@@ -150,5 +150,10 @@ window.addEventListener('scroll', () => {
   });
 });
 
+// Dynamically update the year in the footer
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("current-year").textContent = currentYear;
+});
 
 
